19ec9eab4af2a8631bc564d288ccaed90767e0c1
Choyoungjun <cyj070513@gmail.com>
2023-06-12 18:59:07 +0900

    복구-2

--- RADO/RADO/StartingPageViewController.swift
+++ RADO/RADO/StartingPageViewController.swift
@@ -58,19 +58,17 @@ class StartingPageViewController: UIViewController {
         mainName.snp.makeConstraints {
             $0.top.equalToSuperview().inset(466)
             $0.bottom.equalToSuperview().inset(329)
-            $0.left.right.equalToSuperview().inset(145.5)
+            $0.centerX.equalToSuperview()
         }
         loginButton.snp.makeConstraints {
-            $0.top.equalToSuperview().inset(675)
+            $0.top.equalTo(mainName.snp.bottom).offset(160)
             $0.left.right.equalToSuperview().inset(42.5)
             $0.height.equalTo(45)
-            $0.width.equalTo(305)
         }
         signupButton.snp.makeConstraints {
-            $0.top.equalToSuperview().inset(728)
+            $0.top.equalTo(loginButton.snp.bottom).offset(8)
             $0.left.right.equalToSuperview().inset(42.5)
             $0.height.equalTo(45)
-            $0.width.equalTo(305)
         }
         
     }
