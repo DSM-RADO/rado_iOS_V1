93231c2f0a9d1cae68242648fc67fcb70885bf01
Choyoungjun <cyj070513@gmail.com>
2023-06-15 19:17:04 +0900

    feedContentView 네비게이션바 설정

--- RADO/RADO/MainFeedContentView/FeedContentViewController.swift
+++ RADO/RADO/MainFeedContentView/FeedContentViewController.swift
@@ -4,24 +4,28 @@ import Then
 
 class FeedContentViewController: UIViewController {
     
+    let profileImage = UIImageView().then {
+        $0.image = UIImage(named: "testImage")
+        $0.layer.cornerRadius = 75
+        $0.clipsToBounds = true
+    }
     let feedContentField = UITextField().then {
         $0.placeholder = "입력하세요"
         $0.font = UIFont.systemFont(ofSize: 15)
-        $0.leftView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 17.0, height: 0.0))
+        $0.leftView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 9.0, height: 0.0))
         $0.leftViewMode = .always
-        $0.rightView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: -17.0, height: 0.0))
+        $0.rightView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: -9.0, height: 0.0))
         $0.rightViewMode = .always
-        $0.backgroundColor = UIColor.red
+        $0.backgroundColor = UIColor.white
         $0.layer.cornerRadius = 10
-        
     }
-//    let textFieldBackground = UILabel().then {
-//        $0.backgroundColor = UIColor(named: "navy")
-//    }
+    
+    let textFieldBackground = UIView().then {
+        $0.backgroundColor = UIColor(named: "navy")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        
     }
     
     override func viewDidLayoutSubviews() {
@@ -29,33 +33,29 @@ class FeedContentViewController: UIViewController {
         makeConstrains()
     }
     
+    
     func addSubView() {
         [
-            feedContentField,
-//            textFieldBackground,
+//            profileImage,
+            textFieldBackground,
         ].forEach({self.view.addSubview($0)})
+        textFieldBackground.addSubview(feedContentField)
     }
     
     func makeConstrains() {
-//        completeButton.snp.makeConstraints {
-//
+        //        profileImage.snp.makeConstraints {
+//            $0.width.equalToSuperview().inset(66)
+//            $0.height.equalToSuperview().inset(64)
 //        }
-//        textFieldBackground.snp.makeConstraints {
-//            $0.centerX.equalToSuperview()
-//                        $0.top.equalToSuperview().offset(50)
-//            //            $0.width.height.equalTo(100)
-//            $0.top.equalToSuperview().inset(764)
-//            $0.width.equalToSuperview()
-//            $0.bottom.equalToSuperview()
-//        }
-        feedContentField.snp.makeConstraints {
-//            $0.top.equalTo(textFieldBackground.snp.bottom).offset(12)
-            $0.top.equalToSuperview().inset(500)
+        textFieldBackground.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(764)
+            $0.width.equalToSuperview()
             $0.bottom.equalToSuperview()
+        }
+        feedContentField.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(12)
+            $0.bottom.equalToSuperview().inset(18)
             $0.left.right.equalToSuperview().inset(11)
-            
-//            $0.edges.equalToSuperview().inset(UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10))
-            
-            }
         }
     }
+}
