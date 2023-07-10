a0db64ff24582af907cd933103a53dc3f87f0dcc
Choyoungjun <cyj070513@gmail.com>
2023-07-10 16:05:16 +0900

    ⚡️ :: Alert 코드 수정

--- RADO/RADO/Component/CustomAlert.swift
+++ RADO/RADO/Component/CustomAlert.swift
@@ -27,6 +27,16 @@ class CustomAlert: UIViewController {
         $0.setTitleColor(UIColor(named: "gray"), for: .normal)
         $0.titleLabel?.font = UIFont.systemFont(ofSize: 14)
     }
+    init(
+        title: String
+    ){
+        super.init(nibName: nil, bundle: nil)
+        self.titleLabel.text = title
+        self.modalPresentationStyle = .overFullScreen
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .clear
@@ -65,9 +75,6 @@ class CustomAlert: UIViewController {
     @objc func clickCloseButton() {
         dismiss(animated: true)
     }
-    func alertMessage(title: String) {
-        titleLabel.text = title
-        self.modalPresentationStyle = .overFullScreen
-    }
+   
 }
 
